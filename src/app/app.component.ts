d4c8b29b39d7cf808cdaebb432a507116665079f
Ro Brett <ro.brett@gmail.com>
2019-01-07 23:08:21 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { ScannerService } from './lexer/scanner.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'query-search';
+
+  constructor(private scanner: ScannerService) {
+
+    this.scanner.setText('fundsgroup IN "Hello"');
+
+    this.scanner.getTokens();
+  }
 }
